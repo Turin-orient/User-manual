3babaa7ecb05fe3a038f793458f2e206c90468d1
Sang Nguyen Anh <sang.nguyenanh@orientsoftware.com>
2026-01-28 17:00:38 +0700

    Version 2

--- .docusaurus/routes.js
+++ .docusaurus/routes.js
@@ -3,51 +3,16 @@ import ComponentCreator from '@docusaurus/ComponentCreator';
 
 export default [
   {
-    path: '/__docusaurus/debug',
-    component: ComponentCreator('/__docusaurus/debug', '5ff'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/config',
-    component: ComponentCreator('/__docusaurus/debug/config', '5ba'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/content',
-    component: ComponentCreator('/__docusaurus/debug/content', 'a2b'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/globalData',
-    component: ComponentCreator('/__docusaurus/debug/globalData', 'c3c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/metadata',
-    component: ComponentCreator('/__docusaurus/debug/metadata', '156'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/registry',
-    component: ComponentCreator('/__docusaurus/debug/registry', '88c'),
-    exact: true
-  },
-  {
-    path: '/__docusaurus/debug/routes',
-    component: ComponentCreator('/__docusaurus/debug/routes', '000'),
-    exact: true
-  },
-  {
     path: '/docs',
-    component: ComponentCreator('/docs', 'd9d'),
+    component: ComponentCreator('/docs', 'adc'),
     routes: [
       {
         path: '/docs',
-        component: ComponentCreator('/docs', '278'),
+        component: ComponentCreator('/docs', '62f'),
         routes: [
           {
             path: '/docs',
-            component: ComponentCreator('/docs', 'fb7'),
+            component: ComponentCreator('/docs', 'fba'),
             routes: [
               {
                 path: '/docs/admin-guide/adding-api-keys',
@@ -297,6 +262,42 @@ export default [
               {
                 path: '/docs/user-guide/using-chat-interface',
                 component: ComponentCreator('/docs/user-guide/using-chat-interface', 'bcb'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/user-manual/chat-ui',
+                component: ComponentCreator('/docs/user-manual/chat-ui', '269'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/user-manual/collection',
+                component: ComponentCreator('/docs/user-manual/collection', '848'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/user-manual/image-studio',
+                component: ComponentCreator('/docs/user-manual/image-studio', '630'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/user-manual/login',
+                component: ComponentCreator('/docs/user-manual/login', '2c9'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/user-manual/prompt-gallery',
+                component: ComponentCreator('/docs/user-manual/prompt-gallery', '57f'),
+                exact: true,
+                sidebar: "tutorialSidebar"
+              },
+              {
+                path: '/docs/user-manual/workspace',
+                component: ComponentCreator('/docs/user-manual/workspace', '228'),
                 exact: true,
                 sidebar: "tutorialSidebar"
               }
